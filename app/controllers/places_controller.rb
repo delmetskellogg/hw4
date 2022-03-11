b61eb617cafabe768c169f6d5aa5a1b070ee4f55
delmetskellogg <david.elmets@kellogg.northwestern.edu>
2022-03-11 06:36:44 +0000

    Username gets added to table

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -15,7 +15,7 @@ class PlacesController < ApplicationController
 
   def create
     @place = Place.new(params["place"])
-    @place.user = @current_user
+    #@place.user = @current_user
     @place.save
     redirect_to "/places"
   end
